a9442573cba37eb3cb9f16b066ae9054fde5161b
Seb Jones <sebowenjones@gmail.com>
2020-02-17 13:42:57 +0000

    Add v2 maths and english lesson journey's, plus question management.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,76 @@ if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   window.GOVUKFrontend.initAll()
 })
+
+function setLocalStorageObject(name, value) {
+  localStorage.setItem(name, JSON.stringify(value))
+}
+
+function getLocalStorageObject(name) {
+  var object = localStorage.getItem(name)
+
+  return object ? JSON.parse(object) : null
+}
+
+function addToLocalStorageObject(name, key, value) {
+  var existing = localStorage.getItem(name)
+
+  existing = existing ? JSON.parse(existing) : {}
+
+  existing[key] = value
+
+  setLocalStorageObject(name, existing)
+}
+
+function addCourseToLocalStorage(name) {
+  var course = {
+    answers: []
+  }
+
+  setLocalStorageObject(name, course)
+}
+
+function addAnswer(courseName, answer) {
+  var course = getLocalStorageObject(courseName)
+  if (!course) {
+    console.log('Could not find course ' + courseName)
+    return
+  }
+
+  var answers = course.answers
+  answers.push(answer)
+  course.answers = answers
+
+  localStorage.setItem(courseName, JSON.stringify(course))
+}
+
+function getAssessmentResults(courseName) {
+  var course = getLocalStorageObject(courseName)
+  if (!course) {
+    console.log(
+      'Could not get score for course ' + courseName + ', course not found.'
+    )
+    return
+  }
+
+  var answers = course.answers
+  if (answers.length <= 0) {
+    return 0
+  }
+
+  var correct = answers.filter(answer => answer === 'correct').length
+  var incorrect = answers.filter(answer => answer === 'incorrect').length
+  var skipped = answers.filter(answer => answer === 'skipped').length
+
+  var percentage = ((correct / answers.length) * 100).toFixed(0)
+
+  return {
+    correct,
+    incorrect,
+    skipped,
+    percentage
+  }
+}
