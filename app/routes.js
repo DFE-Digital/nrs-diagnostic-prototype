107b5d8c8a0ada94a3c6e3c40d7f5420cf752f13
Seb Jones <sebowenjones@gmail.com>
2020-03-16 14:15:38 +0000

    Create v6 branch for content updates.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const v4 = require('./routes-v4')
 const v4_2 = require('./routes-v4_2')
 const v5 = require('./routes-v5')
+const v6 = require('./routes-v6')
 
 const express = require('express')
 let router = express.Router()
@@ -8,5 +9,6 @@ let router = express.Router()
 router = v4.generateRoutesV4(router)
 router = v4_2.generateRoutesv4_2(router)
 router = v5.generateRoutesv5(router)
+router = v6.generateRoutesV6(router)
 
 module.exports = router
