93b424d9e182a17324a8bf02586643f3c36f8f3d
Seb Jones <sebowenjones@gmail.com>
2020-03-16 12:17:34 +0000

    Create v5 branch for initial userlytics testing.

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 const v4 = require('./routes-v4')
 const v4_2 = require('./routes-v4_2')
+const v5 = require('./routes-v5')
 
 const express = require('express')
 let router = express.Router()
 
 router = v4.generateRoutesV4(router)
 router = v4_2.generateRoutesv4_2(router)
+router = v5.generateRoutesv5(router)
 
 module.exports = router
