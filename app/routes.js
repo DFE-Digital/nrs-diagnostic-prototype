52563c9f0c34cc0771edd39502e34d2426e54abb
Seb Jones <sebowenjones@gmail.com>
2020-03-24 13:22:47 +0000

    Create v8-hj (hotjar) branch.

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ const v6 = require('./routes-v6')
 const v6_demo = require('./routes-v6-demo')
 const v7 = require('./routes-v7')
 const v8_ul = require('./routes-v8-ul')
+const v8_hj = require('./routes-v8-hj')
 
 const express = require('express')
 let router = express.Router()
@@ -16,5 +17,6 @@ router = v6.generateRoutesV6(router)
 router = v6_demo.generateRoutesV6Demo(router)
 router = v7.generateRoutesV7(router)
 router = v8_ul.generateRoutesV8_ul(router)
+router = v8_hj.generateRoutesV8_hj(router)
 
 module.exports = router
