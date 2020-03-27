d8bdb82be8ce7f103f0728bf81168f5428d15be8
Seb Jones <sebowenjones@gmail.com>
2020-03-27 10:53:56 +0000

    Move v4.2 routes and layout files.

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 const v4 = require('./routes-v4')
-const v4_2 = require('./routes-v4_2')
+const v4_2 = require('./views/main/v4-2/routes-v4_2')
 const v5 = require('./views/main/v5/routes-v5')
 const v6 = require('./views/main/v6/routes-v6')
 const v6_demo = require('./views/main/v6-demo/routes-v6-demo')
